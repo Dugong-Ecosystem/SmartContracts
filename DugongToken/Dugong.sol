f0d90935f8967f91a67e229ff708d852b733002a
Rajesh Kumar Roy <107270413+RajeshKumarRoy@users.noreply.github.com>
2023-02-08 19:59:56 +0000

    Update Dugong.sol

--- DugongToken/Dugong.sol
+++ DugongToken/Dugong.sol
@@ -1,191 +1,79 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
-contract Dugong
-{
-    address private _owner;
-    address private _bridge;
+contract Dugong {
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+    event Transfer(address indexed sender, address indexed recipient, uint256 amount);
 
+    address private _dugongTreasury;
     string private _name;
     string private _symbol;
     uint8 private _decimals;
     uint256 private _totalSupply;
 
-    mapping(address => uint256) private _balanceOf;
+    mapping(address => uint256) private _balance;
     mapping(address => mapping(address => uint256)) private _allowance;
 
-    bool private _isRunning;
-
-    event Transfer(address indexed sender, address indexed recipient, uint256 amount);
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
-
-    constructor()
-    {
-        _owner = msg.sender;
-
-        _name = "Dugong";
-        _symbol = "DGNG";
+    constructor() {
+        _dugongTreasury = address(0x80840bcEBf90C958E9b34D913b62713B18B994AE);
+        _name = "Dugong Token";
+        _symbol = "SEACOW";
         _decimals = 18;
-        _totalSupply = 1000000 ether;
-
-        _balanceOf[address(this)] = _totalSupply;
-        emit Transfer(address(0), address(this), _totalSupply);
-
-        _allowance[address(this)][_owner] = _totalSupply;
-        emit Approval(address(this), _owner, _totalSupply);
-
-        _isRunning = true;
+        _totalSupply = 1000000 * (10 ** _decimals);
+        
+        _balance[_dugongTreasury] = _totalSupply;
+        emit Transfer(address(this), _dugongTreasury, _totalSupply);
     }
 
-    function name() external view returns (string memory)
-    {
+    function name() external view returns(string memory) {
         return _name;
     }
 
-    function symbol() external view returns (string memory)
-    {
+    function symbol() external view returns(string memory) {
         return _symbol;
     }
 
-    function decimals() external view returns (uint8)
-    {
+    function decimals() external view returns(uint8) {
         return _decimals;
     }
 
-    function totalSupply() external view returns (uint256)
-    {
+    function totalSupply() external view returns(uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address owner) external view returns (uint256)
-    {
-        return _balanceOf[owner];
+    function balanceOf(address owner) external view returns(uint256) {
+        return _balance[owner];
     }
 
-    function allowance(address owner, address spender) external view returns (uint256)
-    {
+    function allowance(address owner, address spender) external view returns(uint256) {
         return _allowance[owner][spender];
     }
 
-    function approve(address spender, uint256 amount) run external returns (bool)
-    {
+    function approve(address spender, uint256 amount) external returns(bool) {
         _allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
-
         return true;
     }
-    
-    function transfer(address recipient, uint256 amount) run external returns (bool)
-    {
-        require(_balanceOf[msg.sender] > 0, "Zero Balance!");
-        require(_balanceOf[msg.sender] >= amount, "Low Balance!");
 
-        _balanceOf[msg.sender] -= amount;
-        _balanceOf[recipient] += amount;
+    function transfer(address recipient, uint256 amount) external returns(bool) {
+        require(_balance[msg.sender] > 0, "Zero Balance.");
+        require(_balance[msg.sender] >= amount, "Low Balance.");
+        _balance[msg.sender] -= amount;
+        _balance[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
-
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) run external returns (bool)
-    {
-        require(_allowance[sender][msg.sender] > 0, "Zero Allowance!");
-        require(_allowance[sender][msg.sender] >= amount, "Low Allowance!");
-        require(_balanceOf[sender] > 0, "Zero Balance!");
-        require(_balanceOf[sender] >= amount, "Low Balance!");
-
+    function transferFrom(address sender, address recipient, uint256 amount) external returns(bool) {
+        require(_allowance[sender][msg.sender] > 0, "Zero Allowance.");
+        require(_allowance[sender][msg.sender] >= amount, "Low Allowance.");
+        require(_balance[sender] > 0, "Zero Balance.");
+        require(_balance[sender] >= amount, "Low Balance.");
         _allowance[sender][msg.sender] -= amount;
-        _balanceOf[sender] -= amount;
-        _balanceOf[recipient] += amount;
+        _balance[sender] -= amount;
+        _balance[recipient] += amount;
         emit Transfer(sender, recipient, amount);
-
         return true;
-    }
-
-    function mint(address to, uint256 amount) run onlyAdmin external returns (bool)
-    {
-        _balanceOf[to] += amount;
-        _totalSupply += amount;
-        emit Transfer(address(0), to, amount);
-
-        return true;
-    }
-
-    function burn(address from, uint256 amount) run onlyAdmin external returns (bool)
-    {
-        require(_balanceOf[from] > 0, "Zero Balance!");
-        require(_balanceOf[from] >= amount, "Low Balance!");
-
-        _balanceOf[from] -= amount;
-        _totalSupply -= amount;
-        emit Transfer(from, address(0), amount);
-
-        return true;
-    }
-
-    function isRunning() external view returns (bool)
-    {
-        return _isRunning;
-    }
-
-    function start() onlyOwner external returns (bool)
-    {
-        require(!_isRunning, "Transaction is already Enabled!");
-
-        _isRunning = !_isRunning;
-
-        return true;
-    }
-
-    function stop() onlyOwner external returns (bool)
-    {
-        require(_isRunning, "Transaction is already Disabled!");
-
-        _isRunning = !_isRunning;
-
-        return true;
-    }
-
-    function setOwner(address newOwner) onlyOwner external returns (bool)
-    {
-        require(newOwner != address(0), "Invalid Address!");
-        _owner = newOwner;
-        return true;
-    }
-
-    function getOwner() external view returns (address)
-    {
-        return _owner;
-    }
-
-    function setBridge(address newBridge) onlyOwner external returns (bool)
-    {
-        require(newBridge != address(0), "Invalid Address!");
-        _bridge = newBridge;
-        return true;
-    }
-
-    function getBridge() external view returns (address)
-    {
-        return _bridge;
-    }
-
-    modifier onlyOwner
-    {
-        require(msg.sender == _owner, "Access Denied, You're not the Owner!");
-        _;
-    }
-
-    modifier onlyAdmin
-    {
-        require((msg.sender == _owner) || (msg.sender == _bridge), "Access Denied, You're not the Admin!");
-        _;
-    }
-
-    modifier run
-    {
-        require(_isRunning, "Transaction is Temporarily Disabled!");
-        _;
     }
 }
